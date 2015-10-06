3e4f3b698a8bbbc13f56238f43ec36f365e9ac65
TPC <tiepcyber@gmail.com>
2015-10-06 18:31:58 +0800

    Add circle view draft indicating calories consumption

--- NutriWiki/app/src/main/java/nutriwiki/holmusk/com/nutriwiki/SummaryActivity.java
+++ NutriWiki/app/src/main/java/nutriwiki/holmusk/com/nutriwiki/SummaryActivity.java
@@ -3,6 +3,7 @@ package nutriwiki.holmusk.com.nutriwiki;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bowyer.app.fabtoolbar.FabToolbar;
+import com.dlazaro66.wheelindicatorview.WheelIndicatorItem;
+import com.dlazaro66.wheelindicatorview.WheelIndicatorView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
@@ -32,8 +35,9 @@ public class SummaryActivity extends AppCompatActivity implements ObservableScro
     @Bind(R.id.ic_logfood)
     ImageView mIcEmail;
     @Bind(R.id.ic_chart)
-
     ImageView mIcForum;
+    @Bind(R.id.wheel_indicator_view)
+    WheelIndicatorView wheelView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,7 @@ public class SummaryActivity extends AppCompatActivity implements ObservableScro
 
 
         ButterKnife.bind(this);
+
         initScrollView();
         //set floating button to FabToolbar
         mFabToolbar.setFab(mFab);
@@ -50,6 +55,17 @@ public class SummaryActivity extends AppCompatActivity implements ObservableScro
     private void initScrollView() {
 
         mObservableListView.setScrollViewCallbacks(this);
+        WheelIndicatorItem breakFastIncatorItem = new WheelIndicatorItem(1.5f , Color.parseColor("#FF5722"));
+        wheelView.addWheelIndicatorItem(breakFastIncatorItem);
+        WheelIndicatorItem lunchIndicatorItem = new WheelIndicatorItem(1.2f, Color.parseColor("#0000FF"));
+        wheelView.addWheelIndicatorItem(lunchIndicatorItem);
+        WheelIndicatorItem dinnerIncatorItem = new WheelIndicatorItem(0.8f, Color.parseColor("#00FF00"));
+        wheelView.addWheelIndicatorItem(dinnerIncatorItem);
+        WheelIndicatorItem othersIncatorItem = new WheelIndicatorItem(0.3f, Color.parseColor("#000000"));
+        wheelView.addWheelIndicatorItem(othersIncatorItem);
+
+        wheelView.notifyDataSetChanged();
+
     }
 
     @Override
