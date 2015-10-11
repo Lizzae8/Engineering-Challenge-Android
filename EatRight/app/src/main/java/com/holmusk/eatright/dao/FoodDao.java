5b23fd5e7b802a592265b56bb64d39dc00f494b0
Kai <wngkai.91@gmail.com>
2015-10-11 21:01:58 +0800

    Moving logging time into FoodDao

--- EatRight/app/src/main/java/com/holmusk/eatright/dao/FoodDao.java
+++ EatRight/app/src/main/java/com/holmusk/eatright/dao/FoodDao.java
@@ -1,8 +1,11 @@
 package com.holmusk.eatright.dao;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.holmusk.eatright.models.Food;
+
+import org.joda.time.DateTime;
 
 import java.util.List;
 
@@ -13,6 +16,7 @@ import io.realm.Realm;
  */
 public class FoodDao {
 
+    private static final String TAG = FoodDao.class.getSimpleName();
     private Realm mRealm;
 
     public FoodDao(Context context){
@@ -20,13 +24,20 @@ public class FoodDao {
     }
 
     public void saveAllFood(List<Food> foods){
+        DateTime now = DateTime.now();
         mRealm.beginTransaction();
         mRealm.copyToRealmOrUpdate(foods);
         mRealm.commitTransaction();
+        Log.d(TAG, "Saving to database took: " + (DateTime.now().getMillis() - now.getMillis())
+                + " milliseconds");
+
     }
 
     public Food searchFoodById(String id){
+        DateTime now = DateTime.now();
         Food food = mRealm.where(Food.class).equalTo("id", id).findFirst();
+        Log.d(TAG, "Retrieving from database took: " + (DateTime.now().getMillis() - now.getMillis())
+                + " milliseconds");
         return food;
     }
 }
